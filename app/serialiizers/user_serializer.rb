b5ffe2c5280069503d266837f1de2f3a70dd5300
jasce <jasce@users.noreply.github.com>
2016-06-17 10:51:01 +0530

    Update user_serializer.rb

--- app/serialiizers/user_serializer.rb
+++ app/serialiizers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email,:locality,:pincode, :created_at, :updated_at, :authentication_token
+  attributes :id,:name, :email,:locality,:pincode, :created_at, :updated_at, :authentication_token
   has_many :bookings
 end
