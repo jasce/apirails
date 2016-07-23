2d93bb57c0799c556ebb05469fe55f1784f1d4b6
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-07-23 17:48:22 +0530

    Added attachments

--- app/admin/booking.rb
+++ app/admin/booking.rb
@@ -28,7 +28,7 @@ index do
 	 column "Attachments" do |booking|
        #booking.attachments.map(&:attachment).join("<br />").html_safe
        booking.attachments.each do |attachment|
-       		link_to attachment
+       		link_to attachment.attachment
        end
   end
   actions
