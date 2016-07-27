43f0f6c055dc520af50f91e3a6196845b283201d
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-07-27 11:29:09 +0530

    Found a way for nested attributes

--- app/controllers/api/v1/s/respond_bookings_controller.rb
+++ app/controllers/api/v1/s/respond_bookings_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::S::RespondBookingsController < Api::V1::BaseApiController
 		   		respond_booking.update(store_category_id: booking.store_category_id)
 		   		UserNotifier.respond_mail(booking.user , booking , respond_booking.store).deliver_now
 		   		SendCode.new.send_sms(:to => booking.user.mobile, :body => "#{respond_booking.store.store_name} responded to your booking with #{respond_booking.discount}% discount  that you created #{time_ago_in_words(booking.created_at)} ago" )
-		      render json: respond_booking,include: :store_category, status: 201#, location: [:api,:v1,:user, booking]
+		      render json: respond_booking, status: 201#, location: [:api,:v1,:user, booking]
 		   else
 		      render json: { errors: respond_booking.errors }, status: 422
 		   end
--- app/serialiizers/respond_booking_serializer.rb
+++ app/serialiizers/respond_booking_serializer.rb
@@ -1,12 +1,14 @@
 class RespondBookingSerializer < ActiveModel::Serializer
 	include ActionView::Helpers::DateHelper
-  attributes :discount,:store_id,:created_at,:store_category_id
+  attributes :discount,:store_id,:created_at,:store_category_id,:user_name
    
   def created_at
     
     time_ago_in_words(object.created_at)
   end
-
+  def user_name
+  	object.booking.user.name
+  end
   	has_one :store_category
   has_one :booking
   has_one :store
